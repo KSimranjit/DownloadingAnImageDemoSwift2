3282a34ea1e5fbb26bcf6f0b70e535ffd587836a
Simranjit <simranjit2701@gmail.com>
2016-02-27 13:30:34 +1100

    Downloading an image from URL Saving it locally Displaying the saved image

--- Downloading An Image/ViewController.swift
+++ Downloading An Image/ViewController.swift
@@ -10,9 +10,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var downloadedImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+       
+            let url = NSURL(string: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c8/Taj_Mahal_in_March_2004.jpg/1920px-Taj_Mahal_in_March_2004.jpg")
+        
+            let task = NSURLSession.sharedSession().dataTaskWithURL(url!) { (data , response, error ) -> Void in
+            
+                if error != nil{
+              
+                    print(error)
+                }else
+                {
+                    //Save the image for offline use
+                    
+                    var documentsDirectory:String?
+                    
+                    var paths:[AnyObject] = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.UserDomainMask, true)
+                    
+                    if paths.count > 0
+                    {
+                        //Code to save image from url and displaying
+                        documentsDirectory = paths[0] as? String
+                        
+                        let savePath = documentsDirectory! + "/taj.jpg"
+                       
+                        //Code for saving image for offline use .
+                        NSFileManager.defaultManager().createFileAtPath(savePath, contents: data , attributes: nil)
+                    
+                        dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                           
+                            self.downloadedImageView.image = UIImage(named: savePath)
+                            //Code for directly displaying from url
+                            //if let taj = UIImage(data: data!){
+                            //    self.downloadedImageView.image = taj
+                            //}
+                        })
+                    
+                    }
+                    
+                }
+            }
+            task.resume()
+            
+        
     }
 
     override func didReceiveMemoryWarning() {
